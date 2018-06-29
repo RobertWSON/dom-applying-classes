7cec24c3b9aec79464c7fa381966c2e6632e1fcd
robjwilkinson@hotmail.com <robert@Roberts-MacBook-Air.local>
2018-06-30 00:48:24 +1200

    3rd div now beige

--- classes.js
+++ classes.js
@@ -9,6 +9,7 @@ function start () {
     
   // Your turn! Create a new function called `two`, then call it from here.
   two()
+  three()
 }
 
 function one () {
@@ -29,5 +30,9 @@ function two () {
 }
 
 // CREATE FUNCTION three HERE
+function three () {
+  var three = document.getElementById('three')
+  three.classList.add('beige')
+}
 
 // CREATE FUNCTION makeVisible HERE
